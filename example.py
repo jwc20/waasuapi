ad9aa20e2f0dce73013cc62493008a24240703c0
jwc20 <choij2015@gmail.com>
2022-08-31 22:15:21 -0700

    added urls

--- example.py
+++ example.py
@@ -1,5 +1,14 @@
 import waasu
 
 client = waasu.WorkAtAStartUp()
+
 client.get_scraped_companies()
 
+c_search_url = "https://www.workatastartup.com/companies?demographic=any&expo=any&hasEquity=any&hasSalary=any&industry=any&interviewProcess=any&jobType=any&layout=list-compact&remote=any&sortBy=keyword&usVisaNotRequired=any"
+company_url = "https://www.workatastartup.com/companies/focal-systems"
+
+print(waasu.is_companies_search(c_search_url)) # => True
+print(waasu.is_company(company_url)) # => True
+
+print(waasu.is_companies_search(company_url)) # => False
+print(waasu.is_company(c_search_url)) # => False
--- waasu/core.py
+++ waasu/core.py
@@ -5,12 +5,39 @@ from bs4 import BeautifulSoup
 
 # Bunch of urls
 
+
+class eBase(str, Enum):
+    URL = "https://www.workatastartup.com"
+
+
+class eHeaders(dict, Enum):
+    PAYLOAD = {"Content-Type": "text/html; charset=UTF-8"}
+
+
+class eCompanies(str, Enum):
+    URL = "companies"
+    DEMOGRAPHIC_URL = "?demographic"
+    EXPO_URL = "expo"
+    EQUITY_URL = "hasEquity"
+    SALARY_URL = "hasSalary"
+    INDUSTRY_URL = "industry"
+    INTERVIEW_PROCESS_URL = "interviewProcess"
+    JOB_TYPE_URL = "jobType"
+    REMOTE_URL = "remote"
+    SORT_BY_URL = "sortBy"
+    VISA_URL = "usVisaNotRequired"
+
+
+class eCompany(str, Enum):
+    URL = "companies"
+
+
 # Helper functions:
 
 
-def is_class_a_url(url):
-    return
+def is_companies_search(url):
+    return True if (eCompanies.URL in url) and ("=" in url) else False
 
 
-def is_class_b_url(url):
-    return
+def is_company(url):
+    return True if (eCompany.URL in url) and ("=" not in url) else False
