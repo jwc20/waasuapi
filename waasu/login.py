39f51eb4e3c3baf2c41904dc7041fceda984a918
jwc20 <choij2015@gmail.com>
2022-09-01 03:49:16 -0700

    selenium logging in

--- waasu/login.py
+++ waasu/login.py
@@ -27,16 +27,29 @@ class LogIn(object):
     def log_in(self):
         print("hello from login page")
         options = Options()
-        options.add_argument("--headless")
-        options.add_argument("--window-size=1980,1020")
-        options.add_argument('--disable-gpu')
+        # options.add_argument("--headless")
+        # options.add_argument("--window-size=1980,1020")
+        # options.add_argument('--disable-gpu')
         
         driver = webdriver.Chrome(options=options)
-        driver.get('http://www.google.com/');
-        time.sleep(5) # Let the user actually see something!
-        search_box = driver.find_element_by_name('q')
-        search_box.send_keys('ChromeDriver')
-        search_box.submit()
+        login_url = "https://account.ycombinator.com/"
+        
+        driver.get(login_url)
+        time.sleep(2) 
+        
+        # YCombinator username
+        yc_username = driver.find_element_by_css_selector('#ycid-input')
+        yc_username.send_keys("test_login") # insert self.username here
+        
+        # YCombinator password
+        yc_password = driver.find_element_by_css_selector('#password-input')
+        yc_password.send_keys("test_password") # insert self.username here
+        
+        submit_button = driver.find_element_by_css_selector('.MuiButton-label')
+        submit_button.click()
+        
+        # search_box.send_keys('ChromeDriver')
+        # search_box.submit()
         time.sleep(5) # Let the user actually see something!
         print("reached the end")
         driver.quit()
