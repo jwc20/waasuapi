eea89f06a191b371ba539889005b1693453ce618
jwc20 <choij2015@gmail.com>
2022-09-01 04:55:20 -0700

    change login page

--- waasu/login.py
+++ waasu/login.py
@@ -6,41 +6,40 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 class LogIn(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
 
     def log_in(self):
         options = Options()
         # options.add_argument("--headless")
         # options.add_argument("--window-size=1980,1020")
         # options.add_argument('--disable-gpu')
-        
+
         driver = webdriver.Chrome(options=options)
-        login_url = "https://account.ycombinator.com/"
-        
+        # login_url = "https://account.ycombinator.com/"
+
+        login_url = "https://account.ycombinator.com/?continue=https%3A%2F%2Fwww.workatastartup.com%2F"
+
         driver.get(login_url)
-        time.sleep(2) 
-        
+        time.sleep(2)
+
         # YCombinator username
-        yc_username = driver.find_element_by_css_selector('#ycid-input')
-        yc_username.send_keys(self.username) # insert self.username here
-        
+        yc_username = driver.find_element_by_css_selector("#ycid-input")
+        yc_username.send_keys(self.username)  # insert self.username here
+
         # YCombinator password
-        yc_password = driver.find_element_by_css_selector('#password-input')
-        yc_password.send_keys(self.password) # insert self.username here
-        
-        submit_button = driver.find_element_by_css_selector('.MuiButton-label')
+        yc_password = driver.find_element_by_css_selector("#password-input")
+        yc_password.send_keys(self.password)  # insert self.username here
+
+        submit_button = driver.find_element_by_css_selector(".MuiButton-label")
         submit_button.click()
-        
-        # search_box.send_keys('ChromeDriver')
-        # search_box.submit()
-        time.sleep(5) # Let the user actually see something!
-        print("reached the end")
-        driver.quit()
+        # driver.quit()
+        # time.sleep(10)
 
+        # print("hellow asdijaisdl")
 
-# if __name__ == "__main__":
-#     LogIn().log_in()
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        print(soup.find_all("span", {"class": "company-name hover:underline"}))
