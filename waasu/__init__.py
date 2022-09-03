a1bb07006888265ca4b905ca491bbbe3b1e3beb4
jwc20 <choij2015@gmail.com>
2022-09-03 05:54:01 -0700

    deleted company.py since layout=list in url will bring up all the details

--- waasu/__init__.py
+++ waasu/__init__.py
@@ -1,13 +1,11 @@
 from .core import *
 from .companies import Companies
-from .company import Company
 from .login import LogIn
 
 
-class WorkAtAStartUp(Companies, Company, LogIn):
+class WorkAtAStartUp(Companies, LogIn):
     def __init__(self, keywords=[], *args):
         Companies.__init__(self, keywords=keywords, *args)
-        Company.__init__(self)
         LogIn.__init__(self, *args)
 
 
