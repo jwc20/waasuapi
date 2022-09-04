b074c2e6338bb5817dec9452bd44c478772fa2e9
jwc20 <choij2015@gmail.com>
2022-09-04 12:28:17 -0700

    update

--- example.py
+++ example.py
@@ -2,17 +2,24 @@ import waasu
 from bs4 import BeautifulSoup
 
 
-query = ["query1", "query2"]
+
 
 # Log In
 # username = "example_username"
 # password = "example_password"
 
 
-client = waasu.WorkAtAStartUp(query=query)
+client = waasu.WorkAtAStartUp()
 client.log_in(username=username, password=password)
 
 # some parameters can have more than one choice
-client.get_companies(companySize="seed", jobType="contract", role="eng", demographic="black-founders", scroll_delay=10)
+# client.get_companies(
+#     jobType="contract", role="eng", demographic="black-founders", scroll_delay=10
+# )
+
+# query = ["junior"]
+query = ["python", "javascript"]
+
+client.get_companies(query=query, jobType="contract", role="eng", scroll_delay=10)
 
 # breakpoint()
