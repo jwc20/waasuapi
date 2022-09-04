63c5b2c45025535f32bd884a9f9d9c200118e821
jwc20 <choij2015@gmail.com>
2022-09-03 17:10:16 -0700

    added options for driver

--- waasu/__init__.py
+++ waasu/__init__.py
@@ -5,12 +5,17 @@ from .login import LogIn
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--window-size=500,1020")
+options.add_argument('--disable-gpu')
 
 class WorkAtAStartUp(Companies, LogIn):
     def __init__(self, query=[], *args):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(options=options)
         Companies.__init__(self, query=query, *args)
         LogIn.__init__(self, *args)
+
 
 
 __authors__ = ["jwc20"]
